9b580155c0a7f7ab0be9cc526d639e322b5f9be5
Chase <eric.nalley+github@gmail.com>
2021-12-13 14:09:05 -0600

    updated for title input
    streamlined css selector to grab title input

--- injector.js
+++ injector.js
@@ -1,26 +1,33 @@
 chrome.storage.sync.get(['outputTitle'], ({ outputTitle }) => {
-    document.getElementById('title').value = outputTitle;
+    const selector = 'input[type="text"][name="instance/brief.description"]';
 
-    var iframe = document.getElementsByTagName('iframe');
+    document.querySelectorAll('iframe').forEach((iframe) => {
+        (iframe.contentWindow || iframe.contentDocument)?.document?.querySelector(selector)?.value = outputTitle;
+    });
+});
 
-    if (iframe.length > 0) {
-        for (var index in iframe) {
-            if (!isNaN(index)) {
-                var specific_iframe = iframe[index].contentWindow || iframe[index].contentDocument;
-                var input = specific_iframe.document.getElementsByTagName('input');
-                var select = specific_iframe.document.getElementsByTagName('select');
-                var textarea = specific_iframe.document.getElementsByTagName('textarea');
-
-                for (var i in iframe) {
-                    if (iframe[i] > 0) {
-                        for (var index in input) {
-                            if (input.length > 0) {
-                                if (input[index].name == 'instance/brief.description') input[index].value = value;
+/*
+    chrome.storage.sync.get(['outputTitle'], ({ outputTitle }) => {
+        var iframe = document.getElementsByTagName('iframe');
+        if (iframe.length > 0) {
+            for (var index in iframe) {
+                if (!isNaN(index)) {
+                    var specific_iframe = iframe[index].contentWindow || iframe[index].contentDocument;
+                    var input = specific_iframe.document.getElementsByTagName('input');
+                    var select = specific_iframe.document.getElementsByTagName('select');
+                    var textarea = specific_iframe.document.getElementsByTagName('textarea');
+        
+                    for (var i in iframe) {
+                        if (iframe[i] > 0) {
+                            for (var index in input) {
+                                if (input.length > 0) {
+                                    if (input[index].name == 'instance/brief.description') input[index].value = value;
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
-});
+    });
+*/
