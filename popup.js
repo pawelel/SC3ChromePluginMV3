bf627f48dab83f491f6413a1e6894d600f588148
Chase <eric.nalley+github@gmail.com>
2021-12-13 12:35:33 -0600

    refactor & fix
    fixed manifest and v3 chrome extension api

--- popup.js
+++ popup.js
@@ -1,193 +1,100 @@
 ﻿/*jshint esversion: 6 */
-var device;
-chrome.runtime.sendMessage({ name: 'fetchAssets' }, (assetList) => {
-  function generateTableHead(table, data) {
-    let thead = table.createTHead();
+
+const assetTable = document.querySelector('#assetTable');
+const searchField = document.querySelector('#searchField');
+const fillForm = document.querySelector('#fillForm');
+const outputTitle = document.querySelector('#output-title');
+const situationSelect = document.querySelector('#situation-select');
+
+function generateTableHead(tableElement, headData) {
+    let thead = tableElement.createTHead();
     let row = thead.insertRow();
-    for (let key of data) {
-      let th = document.createElement('th');
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
+    for (let key of headData) {
+        let th = document.createElement('th');
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
     }
-  }
-  function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      row.classList.add('selectme');
-      for (let key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-  }
-var situationId;
-var area;
-var place;
-var coordinate;
-var model;
-var asset;
-  let table = document.querySelector('#assetTable');
-let title = document.querySelector('#output-title');
-  let data = Object.keys(assetList[0]);
-  generateTableHead(table, data);
-  generateTable(table, assetList);
-  function filterTable() {
-    var input, filter, table, tr, td, i;
-    table = document.querySelector('#assetTable');
-    input = document.getElementById('searchField');
-    filter = input.value.toUpperCase();
-    var rows = table.getElementsByTagName('tr');
-    for (i = 1; i < rows.length; i++) {
-      var cells = rows[i].getElementsByTagName('td');
-      var j;
-      var rowContainsFilter = false;
-      for (j = 0; j < cells.length; j++) {
-        if (cells[j]) {
-          if (cells[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            rowContainsFilter = true;
-            continue;
-          }
-        }
-      }
+}
 
-      if (!rowContainsFilter) {
-        rows[i].style.display = 'none';
-      } else {
-        rows[i].style.display = '';
-      }
+function generateTableBody(tableElement, bodyData) {
+    let tbody = tableElement.createTBody();
+    for (let rowData of bodyData) {
+        let row = tbody.insertRow();
+        row.classList.add('selectme');
+        for (let key in rowData) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(rowData[key]);
+            cell.appendChild(text);
+        }
     }
-  }
-  $('.selectme').on('click', function () {
-    $(this).addClass('selected').siblings().removeClass('selected');
-    device = $(this).find('td:nth-child(4)').text();
-     area = $(this).find('td:first-child').text();
-     place = $(this).find('td:nth-child(2)').text();
-     coordinate = $(this).find('td:nth-child(3)').text();
-     model = $(this).find('td:nth-child(5)').text();
-     asset = $(this).find('td:nth-child(6)').text();
-title.value='';
-    chrome.runtime.sendMessage({ name: 'fetchSituations' }, (situationList) => {
-      console.log(situationList);
-      function generateOptions() {
-        let select = document.getElementById('situation-select');
-        // clear the options
-    while (select.firstChild) {
-      select.removeChild(select.firstChild);
-  }
-  let start = document.createElement('option');
-          start.value = '';
-          start.text = 'Select a situation';
-          select.add(start);
-        for (let situation of situationList) {
-          if (situation.deviceName === device) {
-            let option = document.createElement('option');
-            option.text = situation.name;
+}
+
+function filterTable(tableRows, filter) {
+    tableRows.forEach((row) => (row.innerText.toUpperCase().includes(filter.toUpperCase()) ? (row.style.display = '') : (row.style.display = 'none')));
+}
+
+function generateOptions(situationList, selectedDevice) {
+    const options = [];
+    for (const situation of situationList) {
+        if (selectedDevice === situation.deviceName) {
+            const option = document.createElement('option');
             option.value = situation.situationId;
-            select.add(option);
-          }
+            option.innerHTML = situation.name;
+            options.push(option);
         }
-      }
-      generateOptions();
-      document
-        .querySelector('.selected')
-        .addEventListener('change', generateOptions);
+    }
+    return options;
+}
+
+function setSituation(columnMap) {
+    outputTitle.value = '';
+    const situationName = situationSelect?.options[situationSelect?.selectedIndex]?.text;
+    if (situationName && columnMap('device')) {
+        outputTitle.value = `[${columnMap('area')}] [${columnMap('place')}] [${columnMap('coordinate')}] [${columnMap('model')}] [${columnMap('asset')}] [${situationName}]`;
+    }
+}
+
+function rowClickHandler(selectedRow, columnMap, tableRows) {
+    tableRows.forEach((row) => row.classList.remove('selected'));
+    selectedRow.classList.add('selected');
+
+    // TODO: This should really be done with the asset fetching one time if the filter isn't sent.
+    chrome.runtime.sendMessage({ name: 'fetchSituations' }, (situationList) => {
+        Array.from(situationSelect.children).forEach((child) => child.remove());
+        generateOptions(situationList, columnMap('device')).forEach((option) => situationSelect.appendChild(option));
+        situationSelect.addEventListener('change', (event) => setSituation(columnMap));
+        setSituation(columnMap);
     });
-    $('#situation-select').on('change', function () {
-      situationId = $(this).val();
-      let situationName = $(this).find(':selected').text();
-      if(device!=null){
-  title.value = "[" + area + "] " +"["+ place +"] "+ " [" + coordinate + "] " +"["+ model +"] "+ " [" + asset + "] " + " [" + situationName + "]";
-      }
-    });
-  });
- 
-//hello
- 
-  document.querySelector('#searchField').addEventListener('keyup', filterTable);
+}
+
+async function getCurrentTabId() {
+    const queryOptions = { active: true, currentWindow: true };
+    const [currentTab] = await chrome.tabs.query(queryOptions);
+    return currentTab.id;
+}
+
+async function fillFormClickHandler() {
+    chrome.storage.sync.set({ outputTitle: outputTitle.value });
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: await getCurrentTabId() },
+            files: ['injector.js'],
+        },
+        function (injectionResults) {}
+    );
+}
+
+chrome.runtime.sendMessage({ name: 'fetchAssets' }, (assetList) => {
+    const assetKeys = Object.keys(assetList[0]);
+
+    generateTableHead(assetTable, assetKeys);
+    generateTableBody(assetTable, assetList);
+
+    const columnMap = (row) => (name) => row.cells[assetKeys.indexOf(name)].innerText;
+
+    const tableRows = assetTable.querySelectorAll('.selectme');
+    tableRows.forEach((row) => row.addEventListener('click', (clickEvent) => rowClickHandler(row, columnMap(row), tableRows)));
+    searchField.addEventListener('keyup', (keyEvent) => filterTable(tableRows, searchField.value));
+    fillForm.addEventListener('click', (clickEvent) => fillFormClickHandler());
 });
- //when i click on my button
- document.getElementById('fillForm').addEventListener('click', function () {
-  
-  let outputTitle = document.getElementById('output-title').value;
-
-  chrome.scripting.executeScript({
-      //send the value to be used by script
-           
-  }, function () {
-      //run the script in the file injector.js
-      chrome.scripting.executeScript({
-          files: ['injector.js']
-      });
-  });
-  console.log(outputTitle);
-});
-
-// chrome.runtime.sendMessage({ name: 'fetchTable' }, (response) => {
-//   async function loadIntoTable(response, table) {
-//     const tableHead = table.querySelector('thead')
-//     const tableBody = table.querySelector('tbody')
-
-//     //clear the table
-//     tableHead.innerHTML =
-//       '<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>'
-//     tableBody.innerHTML = ''
-//     //add the headers
-//   for(let i = 0; i < 6; i++) {
-//     tableHead.querySelector('tr').innerHTML += `<th>${response[0]}</th>`
-//   }
-// }
-
-//   loadIntoTable(response, document.querySelector('table'))
-// });
-
-////button click
-//document.getElementById('fillForm').addEventListener('click', function () {
-//    //take text from fields
-//    var titleValue = document.getElementById('titlevalue').value;
-//    var descriptionValue = document.getElementById('descriptionvalue').value;
-//    chrome.tabs.executeScript({
-//        //send variables to script
-//        code: `var titleValue = ${titleValue};
-//               var descriptionValue = ${descriptionValue};`
-//    }, function () {
-//        //run the script in the file injector.js
-//        chrome.tabs.executeScript({
-//            file: 'background.js'
-//        });
-//    });
-//});
-
-//let fillData = document.getElementById('fillData');
-
-//fillData.onclick = function (element) {
-//    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//        chrome.tabs.executeScript(
-//            tabs[0].id,
-//            { file: 'inject-scripts.js' });
-//    });
-//};
-
-//chrome.commands.onCommand.addListener(function (command) {
-//    console.log(command);
-//    if (command == 'fill_form') {
-//        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//            chrome.tabs.executeScript(
-//                tabs[0].id,
-//                { file: 'inject-scripts.js' });
-//        });
-//    }
-//});
-
-//multiple options for select
-//function displayUsers(users){
-//   var sel = document.querySelector('.select-text');
-//   users.forEach( function(user){
-
-//     let opt = document.createElement('option');
-//     opt.value=users.id;
-//     opt.innerHTML += user.name
-//     sel.appendChild(opt);
-//   });
-//};
